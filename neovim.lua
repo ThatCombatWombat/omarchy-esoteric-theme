d15c5626d9549305116d31be1ca40bc858fa29f8
ThatCombatWombat <aldenharrison100@gmail.com>
2026-09-14 21:09:10 -0400

    Initial real theme

---
+++ neovim.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "bjarneo/aether.nvim",
+    branch = "v3",
+    name = "aether",
+    priority = 1000,
+    opts = {
+      colors = {
+        bg = "#1a150b",
+        dark_bg = "#130f07",
+        darker_bg = "#0c0a05",
+        lighter_bg = "#282317",
+
+        fg = "#dbd7cd",
+        dark_fg = "#7d7a72",
+        light_fg = "#eeebe3",
+        bright_fg = "#faf8f3",
+        muted = "#636459",
+
+        red = "#9a664a",
+        yellow = "#897a57",
+        orange = "#9c6548",
+        green = "#7d8567",
+        cyan = "#7d8565",
+        blue = "#7d8569",
+        magenta = "#98674c",
+        brown = "#79715f",
+
+        bright_red = "#bb7f5d",
+        bright_yellow = "#a6956c",
+        bright_green = "#97a17e",
+        bright_cyan = "#97a17c",
+        bright_blue = "#97a180",
+        bright_magenta = "#bd7e5a",
+
+        accent = "#a36c4b",
+        cursor = "#faf8f3",
+        foreground = "#dbd7cd",
+        background = "#1a150b",
+        selection = "#413724",
+        selection_foreground = "#faf8f3",
+        selection_background = "#413724",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "aether",
+    },
+  },
+}
